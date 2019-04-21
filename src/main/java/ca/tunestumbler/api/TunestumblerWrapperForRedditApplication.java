55e7a8f8e30baee5f39943a8a3fc915dd9167fd8
CrispiestHashbrown <r_kieu@outlook.com>
2019-04-22 00:06:12 +0100

    first commit

---
+++ src/main/java/ca/tunestumbler/api/TunestumblerWrapperForRedditApplication.java
@@ -0,0 +1,13 @@
+package ca.tunestumbler.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunestumblerWrapperForRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunestumblerWrapperForRedditApplication.class, args);
+	}
+
+}
