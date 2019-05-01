e36b9dd6e52ec2c53f2383e6970716313c4f8086
CrispiestHashbrown <r_kieu@outlook.com>
2019-05-01 05:35:01 +0100

    Encrypted user password using BCryptPasswordEncoder

--- src/main/java/ca/tunestumbler/api/service/impl/UserServiceImpl.java
+++ src/main/java/ca/tunestumbler/api/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ca.tunestumbler.api.service.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import ca.tunestumbler.api.UserRepository;
@@ -19,6 +20,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	SharedUtils sharedUtils;
 
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Override
 	public UserDTO createUser(UserDTO user) {
 
@@ -30,10 +34,8 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(user, userEntity);
 		
 		String publicUserId = sharedUtils.generateUserId(50);
-		// For testing
-		// TODO: Encrypt password and generate public user ID
 		userEntity.setUserId(publicUserId);
-		userEntity.setEncryptedPassword("testUserPassword");
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 
