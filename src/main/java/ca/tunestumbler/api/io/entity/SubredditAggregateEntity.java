9ea5663699d17f777fff817da1d3fc6e30a47577
CrispiestHashbrown <r_kieu@outlook.com>
2019-05-25 12:32:57 +0100

    Added Subreddit Aggregate DTO and Entity
    Added DTO and Entity for aggregation of all the user's subreddits
    from their subscribed subreddits and subscribed multireddits

---
+++ src/main/java/ca/tunestumbler/api/io/entity/SubredditAggregateEntity.java
@@ -0,0 +1,79 @@
+package ca.tunestumbler.api.io.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import ca.tunestumbler.api.shared.dto.MultiredditDTO;
+import ca.tunestumbler.api.shared.dto.SubredditDTO;
+import ca.tunestumbler.api.shared.dto.UserDTO;
+
+@Entity(name = "subreddit_aggregate")
+public class SubredditAggregateEntity implements Serializable {
+	private static final long serialVersionUID = 275525728395895620L;
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Column(nullable = false, unique = true)
+	private String subredditAggregateId;
+
+	@ManyToOne
+	@JoinColumn(name = "users_id")
+	private UserDTO userDTO;
+
+	@ManyToOne
+	@JoinColumn(name = "subreddit_id")
+	private SubredditDTO subredditDTO;
+
+	@ManyToOne
+	@JoinColumn(name = "multireddit_id")
+	private MultiredditDTO multiredditDTO;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getSubredditAggregateId() {
+		return subredditAggregateId;
+	}
+
+	public void setSubredditAggregateId(String subredditAggregateId) {
+		this.subredditAggregateId = subredditAggregateId;
+	}
+
+	public UserDTO getUserDTO() {
+		return userDTO;
+	}
+
+	public void setUserDTO(UserDTO userDTO) {
+		this.userDTO = userDTO;
+	}
+
+	public SubredditDTO getSubredditDTO() {
+		return subredditDTO;
+	}
+
+	public void setSubredditDTO(SubredditDTO subredditDTO) {
+		this.subredditDTO = subredditDTO;
+	}
+
+	public MultiredditDTO getMultiredditDTO() {
+		return multiredditDTO;
+	}
+
+	public void setMultiredditDTO(MultiredditDTO multiredditDTO) {
+		this.multiredditDTO = multiredditDTO;
+	}
+
+}
