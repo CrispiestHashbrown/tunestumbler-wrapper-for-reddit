e1874aaf1c823a1fa6510b370782f4c5ccb0c3eb
CrispiestHashbrown <r_kieu@outlook.com>
2019-05-25 12:29:40 +0100

    Removed first and last name columns from User Entity and DTO

--- src/main/java/ca/tunestumbler/api/shared/dto/UserDTO.java
+++ src/main/java/ca/tunestumbler/api/shared/dto/UserDTO.java
@@ -6,8 +6,6 @@ public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 9002611090405917353L;
 	private long id;
 	private String userId;
-	private String firstName;
-	private String lastName;
 	private String email;
 	private String password;
 	private String encryptedPassword;
@@ -29,22 +27,6 @@ public class UserDTO implements Serializable {
 
 	public void setUserId(String userId) {
 		this.userId = userId;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
 	}
 
 	public String getEmail() {
