cc9fab8ecf09c8e47ffe07f59aff9b7e6b607e1d
CrispiestHashbrown <r_kieu@outlook.com>
2019-07-14 07:46:41 +0100

    Fix denying Reddit the access to /auth/handler endpoint
    - Bug: added fix for /auth/handler route to not need Tunestumbler
    authentication so that the Reddit API can send auth codes

--- src/main/java/ca/tunestumbler/api/security/SecurityConstants.java
+++ src/main/java/ca/tunestumbler/api/security/SecurityConstants.java
@@ -3,10 +3,11 @@ package ca.tunestumbler.api.security;
 import ca.tunestumbler.api.SpringApplicationContext;
 
 public class SecurityConstants {
-	public static final long EXPIRATION_TIME = 864000000; // 10 days
+	public static final long EXPIRATION_TIME = 31556952000L; // 1 year
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
+	public static final String HANDLER_URL = "/auth/handler";
 
 	public static String getTokenSecret() {
 		ApplicationProperties applicationProperties = (ApplicationProperties) SpringApplicationContext
