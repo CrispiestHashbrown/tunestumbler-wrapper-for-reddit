644c1f6eeeaf4ab973541f7aac17b331b8d2c542
CrispiestHashbrown <r_kieu@outlook.com>
2019-11-01 10:39:56 +0100

    Clean up
    - Clean up `AuthValidationService` unused method
    - Clean up `localhost` test urls

--- src/main/java/ca/tunestumbler/api/service/AuthValidationService.java
+++ src/main/java/ca/tunestumbler/api/service/AuthValidationService.java
@@ -8,8 +8,6 @@ import ca.tunestumbler.api.shared.dto.UserDTO;
 public interface AuthValidationService {
 	AuthValidationDTO createAuthState(UserDTO user);
 
-	AuthValidationDTO getAuthState(String stateId);
-
 	AuthValidationDTO updateState(String stateId, String code);
 
 	HttpHeaders createHandlerHeaders(String state, String code);
