a2209434ad7f8f1593a0807093024b71a8fa6de5
CrispiestHashbrown <r_kieu@outlook.com>
2021-01-30 08:21:35 +0000

    Refactored auth service
    - Updated user agent header to reflect current API version
    - Refactored `createAuthState` to take `userId` instead of the entire
     `UserDTO`, since passing the `UserDTO` as an argument is overkill.
    - Separated creating the `AuthValidationEntity` into a separate
    helper method from `createAuthState` impl method so that the code is
    easier to read and test
    - Separated creating response headers into a separate helper method
    so that this can be reused in `createHandlerHeaders` and
    `createRefreshTokenHeaders`, and also so that the response headers
    can now be tested separately
    - Separated constants from `AuthorizationHelpers` into a separate
    constants class, `AuthConstants` so that it is easier to keep track
    and maintain them
    - Created a separate method to check if the Reddit scopes are the
    correct scopes, instead of validating against a hard-coded string

--- src/main/java/ca/tunestumbler/api/service/impl/helpers/AuthorizationHelpers.java
+++ src/main/java/ca/tunestumbler/api/service/impl/helpers/AuthorizationHelpers.java
@@ -1,7 +1,10 @@
 package ca.tunestumbler.api.service.impl.helpers;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -15,23 +18,35 @@ import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.google.common.base.Strings;
+
+import ca.tunestumbler.api.io.entity.AuthValidationEntity;
+import ca.tunestumbler.api.io.repositories.AuthValidationRepository;
 import ca.tunestumbler.api.io.repositories.UserRepository;
 import ca.tunestumbler.api.security.AuthenticationFacade;
 import ca.tunestumbler.api.security.SecurityConstants;
+import ca.tunestumbler.api.shared.SharedUtils;
 import ca.tunestumbler.api.ui.model.response.ErrorMessages;
 import ca.tunestumbler.api.ui.model.response.ErrorPrefixes;
 import ca.tunestumbler.api.ui.model.response.auth.AuthResponseModel;
+import ca.tunestumbler.api.exceptions.AuthValidationServiceException;
 import ca.tunestumbler.api.exceptions.BadRequestException;
 import ca.tunestumbler.api.exceptions.WebRequestFailedException;
 
 @Component
 public class AuthorizationHelpers {
-
+	
 	@Autowired
 	UserRepository userRepository;
 
 	@Autowired
+	AuthValidationRepository authValidationRepository;
+
+	@Autowired
 	AuthenticationFacade authenticationFacade;
+	
+	@Autowired
+	SharedUtils sharedUtils;
 
 	public String getUserIdFromAuth() {
 		String authenticationEmail = authenticationFacade.getAuthentication().getName();
@@ -46,27 +61,46 @@ public class AuthorizationHelpers {
 		}
 	}
 
-	public AuthResponseModel refreshRedditToken(String refreshToken) {
-		String baseUrl = "https://www.reddit.com";
-		String uri = "/api/v1/access_token";
-		String userAgentHeader = "web:ca.tunestumbler.api:v0.0.1 (by /u/CrispiestHashbrown)";
-		String creds = Base64.getEncoder().encodeToString(SecurityConstants.getAuth().getBytes());
-		String authHeader = "Basic " + creds;
+	public AuthValidationEntity createAuthValidationEntity(String userId) {
+		String stateId = sharedUtils.generateStateId(50);
+		String authorizationUrl = AuthConstants.BASE_URL + AuthConstants.AUTH_URI
+				+ "?" + AuthConstants.CLIENT_ID_PARAMETER
+				+ "&" + AuthConstants.RESPONSE_TYPE_PARAMETER
+				+ "&" + AuthConstants.STATE_PARAMETER + stateId
+				+ "&" + AuthConstants.REDIRECT_URI_PARAMETER + AuthConstants.REDIRECT_URI
+				+ "&" + AuthConstants.DURATION_PARAMETER
+				+ "&" + AuthConstants.SCOPE_PARAMETER;
 
+		AuthValidationEntity authValidationEntity = new AuthValidationEntity();
+		
+		authValidationEntity.setStateId(stateId);
+		authValidationEntity.setUserId(userId);
+		authValidationEntity.setLastModified(sharedUtils.getCurrentTime());
+		authValidationEntity.setAuthorizationUrl(authorizationUrl);
+
+		authValidationRepository.save(authValidationEntity);
+
+		return authValidationEntity;
+	}
+	
+	public AuthResponseModel createRedditTokens(String code) {
+		String creds = Base64.getEncoder().encodeToString(SecurityConstants.getAuth().getBytes());
+		String authHeader = AuthConstants.AUTH_HEADER + creds;
 		WebClient client = WebClient
 				.builder()
-					.baseUrl(baseUrl)
-					.defaultHeader(HttpHeaders.USER_AGENT, userAgentHeader)
+					.baseUrl(AuthConstants.BASE_URL)
+					.defaultHeader(HttpHeaders.USER_AGENT, AuthConstants.USER_AGENT_HEADER)
 					.defaultHeader(HttpHeaders.AUTHORIZATION, authHeader)
 					.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 					.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
 				.build();
 		WebClient.UriSpec<WebClient.RequestBodySpec> request = client.method(HttpMethod.POST);
-		WebClient.RequestBodySpec requestUri = request.uri(uri);
+		WebClient.RequestBodySpec requestUri = request.uri(AuthConstants.ACCESS_TOKEN_URI);
 		
 		LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		map.add("grant_type", "refresh_token");
-		map.add("refresh_token", refreshToken);
+		map.add(AuthConstants.GRANT_TYPE_HEADER, AuthConstants.AUTH_CODE_GRANT);
+		map.add(AuthConstants.CODE_HEADER, code);
+		map.add(AuthConstants.REDIRECT_URI_HEADER, AuthConstants.REDIRECT_URI);
 		BodyInserter<MultiValueMap<String, Object>, ClientHttpRequest> inserter = BodyInserters.fromMultipartData(map);
 		return requestUri
 				.body(inserter)
@@ -84,29 +118,58 @@ public class AuthorizationHelpers {
 				.bodyToMono(AuthResponseModel.class)
 				.block();
 	}
+	
+	public Boolean isScopesValid(String scopes) {
+		return scopes.equals(AuthConstants.VALID_SCOPES);
+	}
+	
+	public HttpHeaders createResponseHeaders(String tokenLifetime) {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		List<String> exposedHeaders = new ArrayList<>(Arrays.asList(AuthConstants.REDDIT_LIFETIME_HEADER));
+		responseHeaders.setAccessControlExposeHeaders(exposedHeaders);
+		responseHeaders.set(AuthConstants.REDDIT_LIFETIME_HEADER, tokenLifetime);
+		return responseHeaders;
+	}
 
-	public AuthResponseModel createRedditTokens(String code) {
-		String baseUrl = "https://www.reddit.com";
-		String uri = "/api/v1/access_token";
-		String userAgentHeader = "web:ca.tunestumbler.api:v0.0.1 (by /u/CrispiestHashbrown)";
+	public AuthValidationEntity updateState(String stateId, String code) {
+		if (Strings.isNullOrEmpty(stateId) || Strings.isNullOrEmpty(code)) {
+			throw new AuthValidationServiceException(ErrorPrefixes.AUTH_SERVICE.getErrorPrefix()
+					+ ErrorMessages.BAD_REQUEST.getErrorMessage());
+		}
+
+		AuthValidationEntity authValidationEntity = authValidationRepository.findByStateIdAndValidated(stateId, false);
+
+		if (authValidationEntity == null) {
+			throw new AuthValidationServiceException(ErrorPrefixes.AUTH_SERVICE.getErrorPrefix()
+					+ ErrorMessages.BAD_REQUEST.getErrorMessage());
+		}
+
+		authValidationEntity.setValidated(true);
+		authValidationEntity.setCode(code);
+		authValidationEntity.setLastModified(sharedUtils.getCurrentTime());
+		authValidationRepository.save(authValidationEntity);
+
+		return authValidationEntity;
+	}
+	
+	public AuthResponseModel refreshRedditToken(String refreshToken) {
 		String creds = Base64.getEncoder().encodeToString(SecurityConstants.getAuth().getBytes());
-		String authHeader = "Basic " + creds;
-		String redirectUri = "https://www.tunestumbler.com/";
+		String authHeader = AuthConstants.AUTH_HEADER + creds;
+
 		WebClient client = WebClient
 				.builder()
-					.baseUrl(baseUrl)
-					.defaultHeader(HttpHeaders.USER_AGENT, userAgentHeader)
+					.baseUrl(AuthConstants.BASE_URL)
+					.defaultHeader(HttpHeaders.USER_AGENT, AuthConstants.USER_AGENT_HEADER)
 					.defaultHeader(HttpHeaders.AUTHORIZATION, authHeader)
 					.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 					.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
 				.build();
 		WebClient.UriSpec<WebClient.RequestBodySpec> request = client.method(HttpMethod.POST);
-		WebClient.RequestBodySpec requestUri = request.uri(uri);
+		WebClient.RequestBodySpec requestUri = request.uri(AuthConstants.ACCESS_TOKEN_URI);
 		
 		LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		map.add("grant_type", "authorization_code");
-		map.add("code", code);
-		map.add("redirect_uri", redirectUri);
+		map.add(AuthConstants.GRANT_TYPE_HEADER, AuthConstants.REFRESH_TOKEN_GRANT);
+		map.add(AuthConstants.REFRESH_TOKEN_HEADER, refreshToken);
 		BodyInserter<MultiValueMap<String, Object>, ClientHttpRequest> inserter = BodyInserters.fromMultipartData(map);
 		return requestUri
 				.body(inserter)
